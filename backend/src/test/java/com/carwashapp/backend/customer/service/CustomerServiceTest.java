04b333fad8d022d0d0a2d94e6877d51df9a13c51
joseademar <joseademarti@gmail.com>
2025-09-07 14:18:37 -0300

    refactor: remove unused import from Main class

--- backend/src/test/java/com/carwashapp/backend/customer/service/CustomerServiceTest.java
+++ backend/src/test/java/com/carwashapp/backend/customer/service/CustomerServiceTest.java
@@ -15,7 +15,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
